c5368103533d5f39d3cc4aa58e525ae89cf7125a
Sebastian Machaj <team@blackredstudio.com>
2024-11-28 14:41:25 +0100

    3_7 - routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Container\Attributes\Auth;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [UserController::class, 'index']);
+Route::middleware('guest')->group(function() {
+    Route::get('/', function(Request $request) {
+
+        dd($request);
+
+        return 'Welcome ROUTING!';
+    })->name('login');
+});
+
+Route::middleware('auth')->group(function() {
+    Route::get('/users/{user}', function(Request $request, User $user) {
+
+        dd($user->name);
+    })->name('singleUser');
+
+    Route::get('/users', function() {
+
+        return view('welcome');
+    })->name('users');
+});
+
+Route::redirect('/usersss', '/users');
