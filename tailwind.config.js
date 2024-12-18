106567edfb796b9239bb60030455f71a9672e0d8
Sebastian Machaj <team@blackredstudio.com>
2024-12-18 16:31:10 +0100

    5_2 - Konfiguracja projektu

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,37 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-    ],
+		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+		 './storage/framework/views/*.php',
+		 './resources/**/*.blade.php',
+		 './resources/**/*.js',
+		 './resources/**/*.vue',
+		 "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
+	],
+    darkMode: 'class',
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
             },
         },
+        extend: {
+            fontFamily: {
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: '#1B00BF',
+                secondary: '#E2FFAC',
+                'dark-blue': '#0A0049',
+                gray: '#71717A',
+                'light-gray': '#A1A1AA',
+                silver: '#F4F4F4',
+            }
+        },
     },
-    plugins: [],
+    plugins: [
+		require("daisyui")
+	],
 };
